6f554c0c56e1fa81e08a1f1a1fb62a92e62ae4d5
Hemanthgowda D A <you@example.com>
2024-02-27 11:17:11 +0530

    first commit

---
+++ src/app/sharepage/loginnav/loginnav.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginnavComponent } from './loginnav.component';
+
+describe('LoginnavComponent', () => {
+  let component: LoginnavComponent;
+  let fixture: ComponentFixture<LoginnavComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginnavComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(LoginnavComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/sharepage/loginnav/loginnav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loginnav',
+  templateUrl: './loginnav.component.html',
+  styleUrl: './loginnav.component.css'
+})
+export class LoginnavComponent {
+
+}
